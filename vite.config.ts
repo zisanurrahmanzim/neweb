31243bfb506b0c288b056556bae394e2769b8f48
zisanurrahmanzim <zisanurrahmanzim@gmail.com>
2025-09-28 20:12:45 +0600

    Simplify deployment workflow and update base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/neweb/',
+    base: './',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
