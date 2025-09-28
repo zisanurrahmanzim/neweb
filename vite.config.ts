80b23a7acbcc4888abedbf0177b9f9293b6768aa
zisanurrahmanzim <zisanurrahmanzim@gmail.com>
2025-09-28 19:57:30 +0600

    Add base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/neweb/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
